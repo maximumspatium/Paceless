34024717082e29b0154fc7e47ec73fb475da1b87
Maxim Poliakovski <maximumspatium@googlemail.com>
2020-10-23 10:33:38 +0200

    hlstruct: support nested structures.

--- hlstruct.py
+++ hlstruct.py
@@ -7,7 +7,7 @@ import struct
 
 class StructField:
     '''
-    Descriptor representing a simple structure field
+    Descriptor representing a simple structure field.
     '''
     def __init__(self, format, offset):
         self.format = format
@@ -23,19 +23,23 @@ class StructField:
 
 class StructureMeta(type):
     '''
-    Metaclass that automatically creates StructField descriptors
+    Metaclass that automatically creates StructField descriptors.
     '''
     def __init__(self, clsname, bases, clsdict):
         fields = getattr(self, '_fields_', [])
         byte_order = ''
         offset = 0
         for format, fieldname in fields:
-            if format.startswith(('<','>','!','@')):
-                byte_order = format[0]
-                format = format[1:]
-            format = byte_order + format
-            setattr(self, fieldname, StructField(format, offset))
-            offset += struct.calcsize(format)
+            if isinstance(format, StructureMeta):
+                setattr(self, fieldname, NestedStruct(fieldname, format, offset))
+                offset += format.struct_size
+            else:
+                if format.startswith(('<','>','!','@')):
+                    byte_order = format[0]
+                    format = format[1:]
+                format = byte_order + format
+                setattr(self, fieldname, StructField(format, offset))
+                offset += struct.calcsize(format)
         setattr(self, 'struct_size', offset)
 
 class Structure(metaclass=StructureMeta):
@@ -45,3 +49,24 @@ class Structure(metaclass=StructureMeta):
     @classmethod
     def from_file(cls, f):
         return cls(f.read(cls.struct_size))
+
+class NestedStruct():
+    '''
+    Descriptor representing a nested structure.
+    '''
+    def __init__(self, name, struct_type, offset):
+        self.name = name
+        self.struct_type = struct_type
+        self.offset = offset
+
+    def __get__(self, instance, cls):
+        if instance is None:
+            return self
+        else:
+            data = instance._buffer[self.offset:
+                                self.offset+self.struct_type.struct_size]
+            result = self.struct_type(data)
+            # Save resulting structure back on instance to avoid
+            # further recomputation of this step
+            setattr(instance, self.name, result)
+            return result
